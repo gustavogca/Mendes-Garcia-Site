3e517130702a299d673df9beee1f08e256b91c46
gustavogca <gugu.caleffo@gmail.com>
2025-08-26 12:10:10 -0300

    Segundo commit

--- scripts.js
+++ scripts.js
@@ -5,7 +5,7 @@ document.querySelectorAll('.menuToggle').forEach(function(toggle) {
         document.querySelectorAll('.dropdown-menu').forEach(function(menu) {
             if (menu !== toggle.nextElementSibling) {
                 menu.classList.remove('show');
-                // Remove active das outras setas
+
                 if (menu.previousElementSibling) {
                     menu.previousElementSibling.classList.remove('active');
                 }
@@ -13,6 +13,18 @@ document.querySelectorAll('.menuToggle').forEach(function(toggle) {
         });
 
         toggle.nextElementSibling.classList.toggle('show');
-        toggle.classList.toggle('active'); // <-- Adicione esta linha
+        toggle.classList.toggle('active');
     });
+});
+
+document.addEventListener('click', function(e) {
+
+    if (!e.target.closest('nav')) {
+        document.querySelectorAll('.dropdown-menu').forEach(function(menu) {
+            menu.classList.remove('show');
+        });
+        document.querySelectorAll('.menuToggle').forEach(function(toggle) {
+            toggle.classList.remove('active');
+        });
+    }
 });
